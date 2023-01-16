3677b8915fed7f279b824d4de41540e2186d5c2f
Carlos Eduardo <eduardogomes114@yahoo.com>
2023-01-16 10:10:30 -0300

    adicionando novo input

--- js/index.js
+++ js/index.js
@@ -1,16 +1,28 @@
-function adicionarLinha(){
-    //Obter os valores dos capos de entrada
 
-    var nome = document.getElementById("nome").value
-    var referencia = document.getElementById("Referencia").value
-    var notaFiscal = document.getElementById("notaFiscal").value
-    var quantiade = document.getElementById("quantidade").value
-    var valor = document.getElementById("valor").value
-    var formaDePagamento = document.getElementById("formaDePagamento").value
+    
+    function adicionarLinha(){
+
+        
+        this.valorTotal()
+        
+        
+        this.montarTabela()
+        
+        
+        this.limparCampos()    
+    }
+    
+    function montarTabela(){
+        var nome = document.getElementById("nome").value
+        var referencia = document.getElementById("Referencia").value
+        var notaFiscal = document.getElementById("notaFiscal").value
+        var quantiade = document.getElementById("quantidade").value
+        var valor = document.getElementById("valor").value
+        var numeroCliente = document.getElementById("nDoCliente").value
+        var formaDePagamento = document.getElementById("formaDePagamento").value
 
 
-
-    var table = document.getElementById("tabela");
+        var table = document.getElementById("tabela");
         var row = table.insertRow(-1);
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
@@ -18,10 +30,38 @@ function adicionarLinha(){
         var cell4 = row.insertCell(3);
         var cell5 = row.insertCell(4);
         var cell6 = row.insertCell(5);
+        var cell7 = row.insertCell(6);
+
         cell1.innerHTML = nome;
         cell2.innerHTML = referencia;
         cell3.innerHTML = notaFiscal;
         cell4.innerHTML = quantiade;
         cell5.innerHTML = valor;
-        cell6.innerHTML = formaDePagamento;
-}
+        cell6.innerHTML = numeroCliente;
+        cell7.innerHTML = formaDePagamento;
+    }
+
+    // Para calcular a quantidade de peças iguais X o valor
+    function valorTotal(){
+
+        var quantiade = parseFloat(document.getElementById("quantidade").value);
+        var valor = parseFloat(document.getElementById("valor").value);
+
+        var res = quantiade * valor
+        
+        console.log(res)
+    
+    }
+
+    function limparCampos(){
+        var nome = document.getElementById("nome").value = ''; 
+        var referencia = document.getElementById("Referencia").value = '';
+        var notaFiscal = document.getElementById("notaFiscal").value = '';
+        var quantiade = document.getElementById("quantidade").value = '';
+        var valor = document.getElementById("valor").value = '';
+        var numeroCliente = document.getElementById("nDoCliente").value = '';
+        var formaDePagamento = document.getElementById("formaDePagamento").value = '';
+    }
+
+    
+    
